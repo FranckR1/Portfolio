4faf7bc130672a448447c6fb093c3e054a1a8af6
Franck <Franck.rd1@outlook.fr>
2025-12-16 16:23:06 +0100

    feat : test deploy in vercel for next and sanity and add fetch skill

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,24 @@
 import { GetStaticProps } from 'next';
 
 import { fetchSocials } from '../pages/api/fetchSocials';
+import { fetchSkills } from '../pages/api/fetchSkills';
 
+import type { skillType } from '../types/skillType';
 import type { socialType } from '../types/socialType';
 
 type typeProps = {
+    skills: skillType[];
     socials: socialType[];
 }
 
-export default function Home({ socials }: typeProps) {
+export default function Home({ skills, socials }: typeProps) {
     
-    console.log("Données reçues dans le composant Home:", socials); 
+    console.log("Données reçues dans le composant Home socials:", socials); 
+    console.log("Données reçues dans le composant Home skills:", skills); 
 
     return (
-        <div>
+    <>
+      <div>
             <h1>Page d'Accueil</h1>
             <h2>Test de Données Socials</h2>
             {socials.length > 0 ? (
@@ -26,28 +31,39 @@ export default function Home({ socials }: typeProps) {
                 <p>Aucune donnée récupérée.</p>
             )}
         </div>
+        <div>
+            <h2>Test de Données Skills</h2>
+            {skills.length > 0 ? (
+                <ul>
+                    {skills.map((skill) => (
+                        <li key={skill.skill}>{skill.skill}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Aucune donnée récupérée.</p>
+            )}
+        </div>
+    </>
     );
 }
 
 export const getStaticProps: GetStaticProps<typeProps> = async () => {
-    try {
-        const socials = await fetchSocials();
+ 
+  const skillsPromise: Promise<skillType[]> = fetchSkills();
+  const socialsPromise: Promise<socialType[]> = fetchSocials();
 
-        console.log("Données récupérées avec succès (dans le terminal) :");
-        console.log(socials);
+  let skills: skillType[];
+  let socials: socialType[];
 
-        return {
-            props: {
-                socials,
-            },
-        };
-    } catch (error) {
-        console.error("Erreur lors de la récupération des données :", error);
-        
-        return {
-            props: {
-                socials: [],
-            },
-        };
-    }
+  [
+    skills,
+    socials
+  ] = await Promise.all([skillsPromise, socialsPromise]);
+
+  return {
+    props: {
+      skills,
+      socials,
+    },
+  }
 };
