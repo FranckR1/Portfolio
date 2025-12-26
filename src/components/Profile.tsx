5d5320aa7f7e23c5e74bbe121550e28343877289
Franck <Franck.rd1@outlook.fr>
2025-12-26 18:16:49 +0100

    feat: create profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -21,7 +21,47 @@ const Profile = ({ picture, pictureGit, socials}: propsType) => {
     }
 
     return (
-        <div></div>
+        <div>
+            <div>
+                <div>
+                    <Image
+                        style={{ objectFit: "cover" }}
+                        src={picture.url}
+                        alt={picture.alt}
+                        fill={true}
+                        sizes="(max-width: 788px) 315px ,(max-width: 1100px) 450px, 450px"
+                        priority={true}
+                    />
+                </div>
+                {github && <a href={github.url}>
+                    <Image
+                        style={{ objectFit: "cover" }}
+                        src={pictureGit.url}
+                        alt={pictureGit.alt}
+                        fill={true}
+                        sizes="(max-width: 788px) 315px ,(max-width: 1100px) 450px, 450px"
+                        priority={true}
+                    />
+                </a>}
+                {github && <div>
+                    <Image
+                        style={{ objectFit: "cover" }}
+                        src={github.image.url}
+                        alt={github.image.alt}
+                        fill={true}
+                        sizes="80px"
+                    />
+                </div>}
+                <div>
+                    <h1>
+                        Franck ROY
+                        <span>
+                            Développeur Full Stack Junior
+                        </span>
+                    </h1>
+                </div>
+            </div>
+        </div>
     )
 };
 
