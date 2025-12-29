efb1279607bb1b67bc1a988bdf87476f9f805347
Franck <Franck.rd1@outlook.fr>
2025-12-29 17:51:04 +0100

    feat: add fetch projects

---
+++ src/pages/api/fetchProjects.ts
@@ -0,0 +1,47 @@
+import { client } from '../api/sanityClient';
+
+import type { projectType } from '../../types/projectType';
+import type { skillType } from '../../types/skillType';
+
+const fetchProjects = async () => {
+    const fetchedProjects: projectType[] = await client.fetch(`*[_type == "project"]{
+        projectTitle, description, date, onProgress,
+        mainImage { alt, caption,
+            "url": asset->url,
+            "aspect": asset->metadata.dimensions.aspectRatio,
+            "width": asset->metadata.dimensions.width,
+            "height": asset->metadata.dimensions.height,
+        },
+        images[] { alt, caption,
+            "url": asset->url,
+            "aspect": asset->metadata.dimensions.aspectRatio,
+            "width": asset->metadata.dimensions.width,
+            "height": asset->metadata.dimensions.height,
+        },
+        githubLinks[]{ name, url },
+        skills[] -> { skill, image{alt, caption, "url": asset->url} },
+    }`);
+
+    return fetchedProjects.map((project: projectType) => ({
+        ...project,
+        date: project.date || '',
+        mainImage: {
+            ...project.mainImage,
+            caption: project.mainImage.caption || '',
+        },
+        images: project.images.map((image) => ({
+            ...image,
+            caption: image.caption || '',
+        })),
+        githubLinks: project.githubLinks || [],
+        skills: project.skills.map((skill: skillType) => ({
+            ...skill,
+            image: {
+                ...skill.image,
+                caption: skill.image.caption || '',
+            }
+        })),
+    }))
+}
+
+export { fetchProjects };
