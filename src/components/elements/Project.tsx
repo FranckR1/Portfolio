e99461304f37d77e97bdf891e2a052b64f14cdd3
Franck <Franck.rd1@outlook.fr>
2025-12-30 16:42:50 +0100

    feat: add image and mainImage in element project

--- src/components/elements/Project.tsx
+++ src/components/elements/Project.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Image from 'next/image';
 import type { projectType } from '../../types/projectType';
 
 type propsType = {
@@ -10,12 +10,40 @@ const Project = ({ project }: propsType) => {
 
     const { 
         projectTitle,
+        mainImage,
+        images,
     } = project;
 
     return (
         <div>
             <div>
                 <h3>{projectTitle}</h3>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <Image
+                            style={{ objectFit: "contain" }}
+                            src={mainImage.url}
+                            alt={mainImage.alt || 'Image'}
+                            width={mainImage.width}
+                            height={mainImage.height}
+                        />
+                    </div>
+                    {images.map((image, i) => {
+                        return (
+                            <div key={i}>
+                                <Image
+                                    style={{ objectFit: "contain" }}
+                                    src={image.url}
+                                    alt={image.alt || 'Image'}
+                                    width={image.width}
+                                    height={image.height}
+                                />
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     )
