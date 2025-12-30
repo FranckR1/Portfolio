14a6ff657bb0bbb2c01d5ce8a2b919fbe495d14c
Franck <Franck.rd1@outlook.fr>
2025-12-30 16:57:58 +0100

    feat: add state for date

--- src/components/elements/Project.tsx
+++ src/components/elements/Project.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from '../../styles/Project.module.css';
 import type { projectType } from '../../types/projectType';
@@ -11,14 +11,22 @@ const Project = ({ project }: propsType) => {
 
     const { 
         projectTitle,
+        date,
         mainImage,
         images,
     } = project;
+
+    const [ dateString, setDateString ] = useState('');
+
+    useEffect(() => {
+        !onprogress && setDateString(Intl.DateTimeFormat(undefined, { month: 'long', year: 'numeric' }).format(new Date(date)));   
+    }, [onprogress, date]);
 
     return (
         <div className={styles.container}>
             <div className={styles.titleContainer}>
                 <h3 className={styles.title}>{projectTitle}</h3>
+                {!onprogress && <time>Fini en {dateString}</time>}
             </div>
             <div className={styles.content}>
                 <div className={styles.slider}>
